1b6a53077460cfb0be830f838c6930ee7a3f4d9c
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-01-01 14:29:56 +0330

    Update 6_💰_Swapped_Assets.py
    Fixing the directory for the CSS file.

--- CrossChainMonitoring/pages/6_💰_Swapped_Assets.py
+++ CrossChainMonitoring/pages/6_💰_Swapped_Assets.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title='Swapped Assets - Cross Chain Monitoring', page_ic
 st.title('💰 Swapped Assets')
 
 # Style
-with open('style.css')as f:
+with open('CrossChainMonitoring/style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 # Data Sources
