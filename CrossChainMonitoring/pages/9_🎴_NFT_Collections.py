6b486a5fbdc6105080e0a59f0cb745470ca98433
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-01-01 14:30:05 +0330

    Update 9_🎴_NFT_Collections.py
    Fixing the directory for the CSS file.

--- CrossChainMonitoring/pages/9_🎴_NFT_Collections.py
+++ CrossChainMonitoring/pages/9_🎴_NFT_Collections.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title='NFT Collections - Cross Chain Monitoring', page_i
 st.title('🎴 NFT Collections')
 
 # Style
-with open('style.css')as f:
+with open('CrossChainMonitoring/style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 # Data Sources
