b83541720cb4d98d4879961f9d1293ab7a6b639b
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-01-01 14:29:48 +0330

    Update 3_💸_Transfers.py
    Fixing the directory for the CSS file.

--- CrossChainMonitoring/pages/3_💸_Transfers.py
+++ CrossChainMonitoring/pages/3_💸_Transfers.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title='USDC Transfers - Cross Chain Monitoring', page_ic
 st.title('💸 USDC Transfers')
 
 # Style
-with open('style.css')as f:
+with open('CrossChainMonitoring/style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 # Data Sources
