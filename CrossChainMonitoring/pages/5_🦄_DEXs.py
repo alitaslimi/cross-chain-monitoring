b45fdf5c85b5c5e690c04aabdc9ca3b27aaa4067
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-01-01 14:29:52 +0330

    Update 5_🦄_DEXs.py
    Fixing the directory for the CSS file.

--- CrossChainMonitoring/pages/5_🦄_DEXs.py
+++ CrossChainMonitoring/pages/5_🦄_DEXs.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title='DEXs - Cross Chain Monitoring', page_icon=':bar_c
 st.title('🦄 DEXs')
 
 # Style
-with open('style.css')as f:
+with open('CrossChainMonitoring/style.css')as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 # Data Sources
