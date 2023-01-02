7b48eddbe2a0e359b86bd38cfed9fcef6e5d5cfa
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-01-03 00:18:53 +0330

    Create Home.py

---
+++ CrossChainMonitoring/Home.py
@@ -0,0 +1,14 @@
+# Libraries
+import streamlit as st
+
+# Layout
+st.set_page_config(page_title='Test', page_icon=':bar_chart:', layout='wide')
+st.title('Test')
+
+c1, c2, c3 = st.columns(3)
+with c1:
+    st.info('**Data Analyst: [@AliTslm](https://twitter.com/AliTslm)**', icon="💡")
+with c2:
+    st.info('**GitHub: [@alitaslimi](https://github.com/alitaslimi)**', icon="💻")
+with c3:
+    st.info('**Data: [Flipside Crypto](https://flipsidecrypto.xyz)**', icon="🧠")
