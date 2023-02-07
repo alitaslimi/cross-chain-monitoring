fc8ea84ea6d1a08f0a8d22ce946de5e2263cef9f
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-02-07 19:31:31 +0000

    Add files via upload

--- Home.py
+++ Home.py
@@ -5,42 +5,25 @@ from PIL import Image
 # Confit
 st.set_page_config(page_title='Cross Chain Monitoring Tool', page_icon=':bar_chart:', layout='wide')
 
-# Google Analytics
-st.components.v1.iframe(
-    """
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PQ45JJR2R7"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-PQ45JJR2R7');
-        </script>
-    """,
-    width=1, height=1
-)
-
 # Title
 st.title('Cross Chain Monitoring Tool')
 
 # Content
-c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15 = st.columns(15)
+c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14 = st.columns(14)
 c1.image(Image.open('images/ethereum-logo.png'))
 c2.image(Image.open('images/bsc-logo.png'))
 c3.image(Image.open('images/polygon-logo.png'))
 c4.image(Image.open('images/solana-logo.png'))
 c5.image(Image.open('images/avalanche-logo.png'))
 c6.image(Image.open('images/cosmos-logo.png'))
-c7.image(Image.open('images/algorand-logo.png'))
-c8.image(Image.open('images/near-logo.png'))
-c9.image(Image.open('images/flow-logo.png'))
-c10.image(Image.open('images/thorchain-logo.png'))
-c11.image(Image.open('images/osmosis-logo.png'))
-c12.image(Image.open('images/gnosis-logo.png'))
-c13.image(Image.open('images/optimism-logo.png'))
-c14.image(Image.open('images/arbitrum-logo.png'))
-c15.image(Image.open('images/axelar-logo.png'))
+c7.image(Image.open('images/near-logo.png'))
+c8.image(Image.open('images/flow-logo.png'))
+c9.image(Image.open('images/thorchain-logo.png'))
+c10.image(Image.open('images/osmosis-logo.png'))
+c11.image(Image.open('images/gnosis-logo.png'))
+c12.image(Image.open('images/optimism-logo.png'))
+c13.image(Image.open('images/arbitrum-logo.png'))
+c14.image(Image.open('images/axelar-logo.png'))
 
 st.write(
     """
