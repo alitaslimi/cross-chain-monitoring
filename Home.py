35f19c27b1a2930f5b17c3ed7f7ee04ad9a1909c
Ali Taslimi <121749081+alitaslimi@users.noreply.github.com>
2023-12-24 18:09:50 +0000

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,45 +1,9 @@
 # Libraries
 import streamlit as st
 from PIL import Image
-import pathlib
-import logging
-import shutil
 
 # Confit
 st.set_page_config(page_title='Cross Chain Monitoring Tool', page_icon=':bar_chart:', layout='wide')
-
-# Google Analytics
-# def inject_ga():
-#     GA_ID = "google_analytics"
-
-
-#     GA_JS = """
-#     <!-- Google tag (gtag.js) -->
-#     <script async src="https://www.googletagmanager.com/gtag/js?id=G-PQ45JJR2R7"></script>
-#     <script>
-#     window.dataLayer = window.dataLayer || [];
-#     function gtag(){dataLayer.push(arguments);}
-#     gtag('js', new Date());
-
-#     gtag('config', 'G-PQ45JJR2R7');
-#     </script>
-#     """
-
-#     # Insert the script in the head tag of the static template inside your virtual
-#     index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-#     logging.info(f'editing {index_path}')
-#     soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-#     if not soup.find(id=GA_ID): 
-#         bck_index = index_path.with_suffix('.bck')
-#         if bck_index.exists():
-#             shutil.copy(bck_index, index_path)  
-#         else:
-#             shutil.copy(index_path, bck_index)  
-#         html = str(soup)
-#         new_html = html.replace('<head>', '<head>\n' + GA_JS)
-#         index_path.write_text(new_html)
-
-# inject_ga()
 
 # Outlier warning
 st.warning("""
@@ -51,23 +15,16 @@ st.warning("""
 # Title
 st.title('Cross Chain Monitoring Tool')
 
-# Content
-c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14 = st.columns(14)
-c1.image(Image.open('images/ethereum-logo.png'))
-c2.image(Image.open('images/bsc-logo.png'))
-c3.image(Image.open('images/polygon-logo.png'))
-c4.image(Image.open('images/solana-logo.png'))
-c5.image(Image.open('images/avalanche-logo.png'))
-c6.image(Image.open('images/cosmos-logo.png'))
-c7.image(Image.open('images/near-logo.png'))
-c8.image(Image.open('images/flow-logo.png'))
-c9.image(Image.open('images/thorchain-logo.png'))
-c10.image(Image.open('images/osmosis-logo.png'))
-c11.image(Image.open('images/gnosis-logo.png'))
-c12.image(Image.open('images/optimism-logo.png'))
-c13.image(Image.open('images/arbitrum-logo.png'))
-c14.image(Image.open('images/axelar-logo.png'))
+# Blockchains
+c1, c2, c3, c4, c5, c6 = st.columns(6)
+c1.image(Image.open('images/arbitrum-logo.png'))
+c2.image(Image.open('images/avalanche-logo.png'))
+c3.image(Image.open('images/bsc-logo.png'))
+c4.image(Image.open('images/ethereum-logo.png'))
+c5.image(Image.open('images/optimism-logo.png'))
+c6.image(Image.open('images/polygon-logo.png'))
 
+# Introduction
 st.write(
     """
     The crypto industry continues to progress and its development has never stopped. Contributors
@@ -82,40 +39,28 @@ st.write(
     into that particular network.
 
     All values for amounts, prices, and volumes are in **U.S. dollars** and the time frequency of the
-    analysis was limited to the last **30 days**.
+    analysis is limited to December 2023.
     """
 )
 
+# Methodology
 st.subheader('Methodology')
 st.write(
     """
-    The data for this cross-chain comparison were selected from the [**Flipside Crypto**](https://flipsidecrypto.xyz)
-    data platform by using its **REST API**. These queries are currently set to **re-run every 24 hours** to cover the latest
-    data and are imported as a JSON file directly to each page. The data were selected with a **1 day delay** for all
-    blockchains to be in sync with one another. The codes for this tool are saved and accessible in its 
-    [**GitHub Repository**](https://github.com/alitaslimi/cross-chain-monitoring).
-
-    It is worth mentioning that a considerable portion of the data used for this tool was manually decoded from the raw
-    transaction data on some of the blockchains. Besides that, the names of addresses, DEXs, collections, etc. are also
-    manually labeled. As the queries are updated on a daily basis to cover the most recent data, there is a chance
-    that viewers encounter inconsistent data through the app. Due to the heavy computational power required to execute
-    the queries, and also the size of the raw data being too large, it was not feasible to cover data for a longer period,
-    or by downloading the data and loading it from the repository itself. Therefore, the REST API was selected as the
-    proper form of loading data for the time being.
+    The data for this cross-chain comparison were extracted from the [**Flipside Crypto**](https://flipsidecrypto.xyz)
+    data platform by using its **REST API**. These queries are currently set to return data for only December 2023
+    and are imported as a JSON file directly to each page. Due to the heavy computational power required to execute
+    the queries, and also the size of the raw data being too large, it was not feasible to cover data for a longer
+    period, or by downloading the data and loading it from the repository itself. Therefore, the REST API was
+    selected as the proper form of loading data for the time being. The codes for this tool are saved and accessible
+    in its [**GitHub Repository**](https://github.com/alitaslimi/cross-chain-monitoring).
     """
 )
 
-st.subheader('Future Works')
-st.write(
-    """
-    This tool is a work in progress and will continue to be developed moving forward. Adding other blockchains,
-    more KPIs and metrics, optimizing the code in general, enhancing the UI/UX of the tool, and more importantly,
-    improving the data pipeline by utilizing [**Flipside ShroomDK**](https://sdk.flipsidecrypto.xyz/shroomdk) are
-    among the top priorities for the development of this app. Feel free to share your feedback, suggestions, and
-    also critics with me.
-    """
-)
+# Divider
+st.divider()
 
+# Credits
 c1, c2, c3 = st.columns(3)
 with c1:
     st.info('**Data Analyst: [@AliTslm](https://twitter.com/AliTslm)**', icon="💡")
--- data.py
+++ data.py
@@ -5,81 +5,16 @@ import pandas as pd
 # Data Sources
 # @st.cache(ttl=1000, allow_output_mutation=True)
 def get_data(query):
+    storage_options = {'User-Agent': 'Mozilla/5.0'}
+
     if query == 'Transactions Overview':
-        return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/579714e6-986e-421a-85dd-c32a8b41b25c/data/latest')
-        # arbitrum = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/f06a4840-9310-41c7-b2b2-1c812884d728/data/latest')
-        # avalanche = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/d543922f-c8b6-4dc9-9caf-91954b3573d4/data/latest')
-        # axelar = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/4ff35680-e680-45c0-b6e9-2c63e6f92156/data/latest')
-        # bsc = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/97d2d421-ebda-4dae-87da-6f27b4478e52/data/latest')
-        # cosmos = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/8cac9410-e4de-4cbe-b146-c6f7f1f053dc/data/latest')
-        # ethereum = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/572f2d6e-4b65-47b7-9dd6-be0710c6e024/data/latest')
-        # flow = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/8a0d84a3-bb18-4815-a36e-850c719b10c3/data/latest')
-        # gnosis = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/e26eeb64-7bf2-42cb-9222-2e6d6483d053/data/latest')
-        # near = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/314e9e48-52ef-466c-a848-3e95957ee9fc/data/latest')
-        # optimism = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/72c31b05-ec16-4533-a2ec-409a49bbe492/data/latest')
-        # osmosis = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/20a3e7ff-fcf5-43f6-bc12-90a9544b55a0/data/latest')
-        # polygon = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/8ea10e70-c838-4a94-b1ef-0cb282e5ced1/data/latest')
-        # solana = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/e01d82cc-9b5d-4166-af72-6110f4947140/data/latest')
-        # return pd.concat(
-        #     [arbitrum, avalanche, axelar, bsc, cosmos, ethereum, flow, gnosis, near, optimism, osmosis, polygon, solana]
-        #         ).sort_values('Blockchain').reset_index(drop=True)
+        return pd.read_json('https://flipsidecrypto.xyz/api/v1/queries/9aa15f9c-d52a-4744-92dc-a6c0acb3e856/data/latest', storage_options=storage_options)
 
     elif query == 'Transactions Daily':
-        return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/4e0c69ff-9395-43c1-af49-f590f864d339/data/latest')
-        # arbitrum = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/7b550fd2-39be-465d-9191-a45d8eb2678b/data/latest')
-        # avalanche = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/b076d022-c0a5-4556-abdb-4b993d04d4a4/data/latest')
-        # axelar = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/c9574cb4-1b02-4141-a52c-3a68475adf33/data/latest')
-        # bsc = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/16aa6493-8602-483e-b691-a5067ca47595/data/latest')
-        # cosmos = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/2c7806ff-70db-40e5-832e-35ec080a60e0/data/latest')
-        # ethereum = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/22f763f9-f927-4875-b3e2-8cd750aae442/data/latest')
-        # flow = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/bf921f59-fe45-4e1d-9bc1-3ddea006fd19/data/latest')
-        # gnosis = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/70395b58-ba81-49a5-90fe-6412786e0293/data/latest')
-        # near = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/a8e2c436-327d-4a89-a829-76d3574ef16f/data/latest')
-        # optimism = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/dc627865-c013-49be-bb1f-648bd62c59d1/data/latest')
-        # osmosis = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/d4a7ec49-4e64-4aa2-a378-87104c8a9521/data/latest')
-        # polygon = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/19453ea9-c03f-48e5-b1e9-13742190d551/data/latest')
-        # solana = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/e584be47-893a-460d-94a0-f217efe14fda/data/latest')
-        # return pd.concat(
-        #     [arbitrum, avalanche, axelar, bsc, cosmos, ethereum, flow, gnosis, near, optimism, osmosis, polygon, solana]
-        #         ).sort_values(['Date', 'Blockchain']).reset_index(drop=True)
+        return pd.read_json('https://flipsidecrypto.xyz/api/v1/queries/a466d059-dc51-4562-93c3-ce597108d792/data/latest', storage_options=storage_options)
 
     elif query == 'Transactions Heatmap':
-        return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/9d8d54d4-b700-4d85-af17-8c29aa29d334/data/latest')
-        # arbitrum = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/c6e9e418-1bba-401e-8275-ff71401e9ae2/data/latest')
-        # avalanche = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/ae43bcac-4cfe-4e9d-9eb9-8413e623f544/data/latest')
-        # axelar = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/a8041ee1-5d59-4105-a493-981c392e9e83/data/latest')
-        # bsc = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/2b03f2a3-c48e-41d0-9507-f6dbac69b6ce/data/latest')
-        # cosmos = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/5de98199-0a6e-4062-bf5e-5e1e41ce5696/data/latest')
-        # ethereum = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/3461c085-6949-4822-8eaf-1660ec80faeb/data/latest')
-        # flow = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/90219f8f-7700-4083-9db0-99a3047bca95/data/latest')
-        # gnosis = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/1bd8d28e-ff39-462c-8d0f-7f68143a0e35/data/latest')
-        # near = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/eae96499-833b-4fd0-ac41-2e12d2cfc84a/data/latest')
-        # optimism = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/767aeab2-6af9-4896-81d5-f3c76e0a4092/data/latest')
-        # osmosis = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/040ca9ec-c5e5-4f93-b35f-40a9e365aabc/data/latest')
-        # polygon = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/d84043fc-fc53-4db3-9607-80fec40eae81/data/latest')
-        # solana = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/852b71d4-c08c-4eea-9b11-38ac7cc8a63c/data/latest')
-        # return pd.concat(
-        #     [arbitrum, avalanche, axelar, bsc, cosmos, ethereum, flow, gnosis, near, optimism, osmosis, polygon, solana]
-        #         ).sort_values(['Day', 'Hour', 'Blockchain']).reset_index(drop=True)
-
-    elif query == 'New Users Daily':
-        return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/d81c5861-0792-43ec-9f92-d89fbcf85e79/data/latest')
-        # arbitrum = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/ff167b88-3f93-416f-a145-8dd899473ba4/data/latest')
-        # avalanche = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/478ca10d-e45f-4ef4-bd82-eda76b08911d/data/latest')
-        # axelar = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/27409124-5c7d-4b79-a20d-2c3e6e47af60/data/latest')
-        # bsc = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/37e21782-f2c0-4e58-86e0-97d60fbb2b40/data/latest')
-        # cosmos = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/a71d9d01-661d-4b1e-be7d-f26af9722e09/data/latest')
-        # ethereum = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/6654b8bc-b684-42ef-90bf-0395b8e571dd/data/latest')
-        # flow = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/e2b8e7f6-b19f-4ff6-b16e-d383b9c4df53/data/latest')
-        # gnosis = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/5667daa1-496e-4068-adb3-1b03aa5f8642/data/latest')
-        # near = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/8524fa45-f475-4811-b3ec-e48b65261c48/data/latest')
-        # optimism = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/3f4a8c40-79e2-4667-9fc8-86a04ed34d4f/data/latest')
-        # osmosis = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/4fa61bb5-aae5-4d58-92ef-5a39af629ac2/data/latest')
-        # polygon = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/e1455d7a-ab1f-4b30-834f-85dca80a7d88/data/latest')
-        # solana = pd.read_json('https://api.flipsidecrypto.com/api/v2/queries/73306514-dd1f-4a18-8c55-e1c024520130/data/latest')
-        # return pd.concat(
-        #     [arbitrum, avalanche, axelar, bsc, cosmos, ethereum, flow, gnosis, near, optimism, osmosis, polygon, solana]
-        #         ).sort_values(['Date', 'Blockchain']).reset_index(drop=True)
+        return pd.read_json('https://flipsidecrypto.xyz/api/v1/queries/ddfd570d-3f48-4a07-a746-b6da1a938dd3/data/latest', storage_options=storage_options)
 
     elif query == 'Fee Payers':
         return pd.read_json('https://node-api.flipsidecrypto.com/api/v2/queries/7eae69ea-2387-420d-b4b9-6eceeb5ef22d/data/latest')
